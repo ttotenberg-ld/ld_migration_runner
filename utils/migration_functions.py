fb3e385398e23010056542243f44d0d1e1984d34
Tom Totenberg <ttotenberg@launchdarkly.com>
2023-11-02 11:53:43 -0500

    added comments

--- utils/migration_functions.py
+++ utils/migration_functions.py
@@ -9,6 +9,7 @@ def write_new():
     def randomizer(error_rate):
         new_rate = error_rate['new']
         value = random.randint(1, 1000)
+        # Intentional pause to simulate write latency
         time.sleep(.2)
         if value >= new_rate:
             print("NEW WRITE: SUCCESS")
@@ -22,6 +23,7 @@ def write_old():
     def randomizer(error_rate):
         old_rate = error_rate['old']
         value = random.randint(1, 1000)
+        # Intentional pause to simulate write latency
         time.sleep(.35)
         if value >= old_rate:
             print("OLD WRITE: SUCCESS")
@@ -35,8 +37,10 @@ def read_new():
     def randomizer(error_rate):
         new_rate = error_rate['new']
         value = random.randint(1, 1000)
+        # Intentional pause to simulate read latency
         time.sleep(.2)
         if value >= new_rate:
+            # Add an additional check on succesful read to determine whether this will be consistent with the other read
             consistency_value = random.randint(1, 100)
             if consistency_value <= 99:
                 print("NEW READ: CONSISTENT")
@@ -53,8 +57,10 @@ def read_old():
     def randomizer(error_rate):
         old_rate = error_rate['old']
         value = random.randint(1, 1000)
+        # Intentional pause to simulate read latency
         time.sleep(.4)
         if value >= old_rate:
+            # Add an additional check on succesful read to determine whether this will be consistent with the other read
             consistency_value = random.randint(1, 100)
             if consistency_value <= 99:
                 print("OLD READ: CONSISTENT")
